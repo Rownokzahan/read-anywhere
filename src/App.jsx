0406fdbd104ad840645e34f98487f953cd518803
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-03-30 22:46:44 +0600

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header/Header';
 
 const App = () => {
   return (
-    <>
-
-    </>
+    <div className='max-w-[1440px] mx-auto'>
+    <Header></Header>
+    </div>
   );
 };
 
---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className='flex justify-between p-8 mb-8 border-b border-b-gray'>
+            <a className='text-3xl font-bold' href="/home">Read Anywhere</a>
+            <a href="/profile">
+                <img className="w-10 h-10 rounded-full" src="https://img.freepik.com/free-photo/close-up-photo-amicable-man-black-shirt-posing-his-passport-picture_132075-8192.jpg?w=360&t=st=1680193454~exp=1680194054~hmac=90ab720fb0ffdfcf0baaa212bed4f5088e8d700ab9a9f3ee9a453bc5d95821f9" alt="" />
+            </a>
+        </nav>
+    );
+};
+
+export default Header;
