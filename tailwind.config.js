fb3da425b0f76bdb6668a24eb072ddc1a5e7d136
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-03-30 21:30:15 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'purple': '#6047EC',
+      'gray-dark': '#273444',
+      'gray': '#11111199',
+      'gray-light': '#f3f3f3',
+    },
+  },
+  plugins: [],
+}
