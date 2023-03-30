f1b990aa29454de55abe77764c41e84cca5bf3c1
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-03-30 22:47:51 +0600

    Load data from data.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header/Header';
 
 const App = () => {
+
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => setBlogs(data))
+  }, []);
+
   return (
     <div className='max-w-[1440px] mx-auto'>
     <Header></Header>
